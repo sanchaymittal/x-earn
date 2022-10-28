e873809acadd3d6b03edfec30de1a3a45a996636
sanchaymittal <sanchaymittal@gmail.com>
2022-10-28 23:11:08 +0100

    update import

--- script/Source.s.sol
+++ script/Source.s.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.14;
 
 import "forge-std/Script.sol";
-import {IConnextHandler} from "nxtp/core/connext/interfaces/IConnextHandler.sol";
-import "../src/contract-to-contract-interactions/with-calldata/Source.sol";
+import {IConnext} from "nxtp/core/connext/interfaces/IConnext.sol";
+import "../src/contracts/Source.sol";
 
 contract DeploySource is Script {
   function run(address _connext, address _promiseRouter) external {
