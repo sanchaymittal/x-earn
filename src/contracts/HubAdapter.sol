0b26a27a54fd1746695b7626710bfc9eceed217f
sanchaymittal <sanchaymittal@gmail.com>
2022-10-29 11:29:16 +0100

    update HubAdapter

--- src/contracts/HubAdapter.sol
+++ src/contracts/HubAdapter.sol
@@ -1,59 +1,58 @@
-
-
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.14;
 
 import {IConnext} from "nxtp/core/connext/interfaces/IConnext.sol";
+import {Cellar} from "cellar/src/base/Cellar.sol";
 
 /**
  * @title HubAdapters
  * @notice TODO
  */
 contract HubAdapter {
-  event UpdateInitiated(address to, uint256 newValue, bool authenticated);
+    IConnext public immutable connext;
 
-  IConnext public immutable connext;
+    Cellar public immutable celler;
 
-  address public delegate;
+    error HubAdapter__onlyCellar_notCellar();
 
+    // A modifier for authenticated function calls.
+    // Note: This is an important security consideration. If your target
+    //       contract function is meant to be authenticated, it must check
+    //       that the originating call is from the correct domain and contract.
+    //       Also, it must be coming from the Connext Executor address.
+    modifier onlyCeller() {
+        if (msg.sender != address(cellar))
+            revert HubAdapter__onlyCellar_notCellar();
+        _;
+    }
+    event UpdateInitiated(address to, uint256 newValue, bool authenticated);
 
-  constructor(IConnext _connext, address _delegate) {
-    connext = _connext;
-    delegate = _delegate;
-  }
+    constructor(IConnext _connext, ICellar _cellar) {
+        connext = _connext;
+        cellar = _cellar;
+    }
 
-  /**
+    /**
    * Cross-domain update of a value on a target contract.
    @dev Initiates the Connext bridging flow with calldata to be used on the target contract.
    */
-  function xChainUpdate(
-    uint32 destination,
-    address to,
-    address asset,
-    uint256 amount
-  ) external payable {
-    // bytes memory callData = abi.encodeWithSelector(selector, );
-    bytes memory callData = "0x";
+    function xCallTargetAdapter(
+        uint32 destination,
+        address asset,
+        uint256 amount,
+        address to,
+        bytes memory callData
+    ) external payable onlyCeller {
+        // function xcall(
+        //   uint32 _destination,
+        //   address _to,
+        //   address _asset,
+        //   address _delegate,
+        //   uint256 _amount,
+        //   uint256 _slippage,
+        //   bytes calldata _callData
+        // ) external payable returns (bytes32);
 
-
-  // function xcall(
-  //   uint32 _destination,
-  //   address _to,
-  //   address _asset,
-  //   address _delegate,
-  //   uint256 _amount,
-  //   uint256 _slippage,
-  //   bytes calldata _callData
-  // ) external payable returns (bytes32);
-
-    connext.xcall(
-      destination,
-      to,
-      asset,
-      delegate,
-      amount,
-      0,
-      callData
-    );
-  }
+        connext.xcall(destination, to, asset, address(0), amount, 0, callData);
+    }
 }
