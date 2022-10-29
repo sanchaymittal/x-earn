c4a05ea67403a99eeac54bc8fe3134e3e54bc764
sanchaymittal <sanchaymittal@gmail.com>
2022-10-29 11:29:23 +0100

    update script

--- script/HubAdapter.s.sol
+++ script/HubAdapter.s.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.15;
 
 import "forge-std/Script.sol";
 import {IConnext} from "nxtp/core/connext/interfaces/IConnext.sol";
 import "../src/contracts/HubAdapter.sol";
 
 contract DeploySource is Script {
-  function run(address _connext, address _delegate) external {
+  function run(address _connext, address _cellar) external {
     vm.startBroadcast();
 
-    IConnext connext = IConnext(_connext);
-    new HubAdapter(connext, _delegate);
+    IConnext connext = IConnext(_connext, _cellar);
+    new HubAdapter(connext);
 
     vm.stopBroadcast();
   }
